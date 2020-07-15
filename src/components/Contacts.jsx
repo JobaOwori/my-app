d84cda9a4727ca02477ec2f7ae153aa079e1726c
joba <oworijoabcollines@gmail.com>
2020-07-15 21:05:43 +0300

    Steady progress

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contacts extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <img src=".\public\IMG-20160409-WA0001.jpg" alt="Photo of me" />
+        <ul></ul>
+        <ul></ul>
+        <ul></ul>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
